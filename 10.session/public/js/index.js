8de5318e9b740f8948e1817deb788fbd886977ae
hkp <sxhkp163@163.com>
2020-09-16 15:27:54 +0800

    session

---
+++ 10.session/public/js/index.js
@@ -0,0 +1,32 @@
+login.onclick = function () {
+  fetch("http://localhost:5008/api/admin/login", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      loginId: "admin",
+      loginPwd: "123123",
+    }),
+  })
+    .then((resp) => resp.json())
+    .then((resp) => {
+      console.log(resp);
+    });
+};
+
+updateStu.onclick = function () {
+  fetch("http://localhost:5008/api/student/1062", {
+    method: "PUT",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      name: "123",
+    }),
+  })
+    .then((resp) => resp.json())
+    .then((resp) => {
+      console.log(resp);
+    });
+};
